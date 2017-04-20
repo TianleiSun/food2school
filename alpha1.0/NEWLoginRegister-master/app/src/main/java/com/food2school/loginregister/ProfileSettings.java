c207d8992b4418e0a5066b05bea26778f1e7efa4
Tianlei Sun <tianleisun@Tianleis-MacBook-Pro.local>
2017-04-20 16:49:24 -0700

    add UI for main page and profilesetting page

--- alpha1.0/NEWLoginRegister-master/app/src/main/java/com/food2school/loginregister/ProfileSettings.java
+++ alpha1.0/NEWLoginRegister-master/app/src/main/java/com/food2school/loginregister/ProfileSettings.java
@@ -1,16 +1,13 @@
 package com.food2school.loginregister;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.EditText;
-import android.widget.TextView;
 
-public class UserAreaActivity extends AppCompatActivity {
+public class ProfileSettings extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_user_area);
-
+        setContentView(R.layout.activity_profile_settings);
     }
 }
