5870ad110ea5851b0b0627a356ff613367dd644b
Tianlei Sun <tianleisun@Tianleis-MacBook-Pro.local>
2017-04-20 17:11:24 -0700

    fix title problem

--- alpha1.0/NEWLoginRegister-master/app/src/main/java/com/food2school/loginregister/UserAreaActivity.java
+++ alpha1.0/NEWLoginRegister-master/app/src/main/java/com/food2school/loginregister/UserAreaActivity.java
@@ -11,6 +11,5 @@ public class UserAreaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_area);
-
     }
 }
